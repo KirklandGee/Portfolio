376d3058f0cc0777eb880f73a2ca21bad512e8ec
KirklandGee <91338420+KirklandGee@users.noreply.github.com>
2022-01-29 17:55:16 -0500

    Portfolio project, JS not linking

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let roleChanger = document.getElementById("changeRole")
+
+roleChanger.addEventListener("click", () => {
+    document.getElementById('roleTitle')
+        .innerHTML = "SEO Campaign Manager"    
+});
